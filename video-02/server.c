a485c5b549b697bbb7e45a1a7e59022ee64910a4
Cameron Kirk <kirkster96@gmail.com>
2023-03-28 17:48:29 -0700

    initial commit

---
+++ video-02/server.c
@@ -0,0 +1,14 @@
+// Server side C/C++ program to demonstrate Socket
+// programming
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+int main(int argc, char const* argv[])
+{
+	
+	return 0;
+}
